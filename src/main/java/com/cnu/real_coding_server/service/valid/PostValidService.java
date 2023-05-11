08fcb377038952770b0e216b74e83a446b518775
kimhyeonju <hyeonju0014@naver.com>
2023-05-11 15:56:16 +0900

    PR

--- src/main/java/com/cnu/real_coding_server/service/valid/PostValidService.java
+++ src/main/java/com/cnu/real_coding_server/service/valid/PostValidService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostValidService {
+public class PostValidService { 
     public boolean isSlangInclude(List<String> slangList,
                                String title,
                                String postContent) {
